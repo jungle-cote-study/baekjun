7831c159305f4dbf2eee9ab35bb8a5f886eba8fc
Haebin <hbtopkr@gmail.com>
2023-03-31 11:57:00 +0900

    0331 Z 정답

--- 조해빈/1074.py
+++ 조해빈/1074.py
@@ -12,13 +12,13 @@ def conq(n, x, y):
 
         if x<quarter and y<quarter:   # 1st square
             cnt += quarter*quarter*0
-        elif x<quarter and y>quarter: # 2nd square
+        elif x<quarter and y>=quarter: # 2nd square
             cnt += quarter*quarter*1
             y -= quarter
-        elif x>quarter and y<quarter: # 3rd square
+        elif x>=quarter and y<quarter: # 3rd square
             cnt += quarter*quarter*2
             x -= quarter
-        else:                         # 4th square
+        else:                           # 4th square
             cnt += quarter*quarter*3
             x -= quarter
             y -= quarter
@@ -27,3 +27,35 @@ def conq(n, x, y):
 
 result = conq(N, r, c)
 print(result)
+
+# 오답 - x, y의 경계값을 고려하지 않았음
+
+# import sys
+# input = sys.stdin.readline
+# N, r, c = map(int, input().split())
+# cnt = 0
+
+# def conq(n, x, y):
+#     global cnt
+
+#     while n>0:
+#         n-=1
+#         quarter = 2**n
+
+#         if x<quarter and y<quarter:   # 1st square
+#             cnt += quarter*quarter*0
+#         elif x<quarter and y>quarter: # 2nd square
+#             cnt += quarter*quarter*1
+#             y -= quarter
+#         elif x>quarter and y<quarter: # 3rd square
+#             cnt += quarter*quarter*2
+#             x -= quarter
+#         else:                         # 4th square
+#             cnt += quarter*quarter*3
+#             x -= quarter
+#             y -= quarter
+
+#     return cnt
+
+# result = conq(N, r, c)
+# print(result)
