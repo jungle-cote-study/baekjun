d4799672ef64f4e5671172d35e99425b2cb9dbc3
bimo <109029169+jihi32123@users.noreply.github.com>
2023-04-01 13:40:29 +0900

    13844 : 삭삽정렬
    정답 코드

--- 박지희/13844.py
+++ 박지희/13844.py
@@ -4,20 +4,18 @@ import sys
 T = int(sys.stdin.readline())
 for _ in range(T):
     testCaseN, N = map(int,sys.stdin.readline().split())
-    
-    l = list(map(int,sys.stdin.readline().split()))
+    l = []
+    for i in range(N//10):
+        l += list(map(int,sys.stdin.readline().split()))
+    if N % 10:
+        l += (list(map(int,sys.stdin.readline().split())))
     sortL = l[0:]
     sortL.sort()
-    
-    max1 = l[0]
     count = 0
     result = 0
-    # print(sortL)
     
-    # while count < len(l)-1 :
     count = 0
     for i in range(0,len(l)):
-        # print(l[i], sortL[count])
         if l[i] > sortL[count]:
             result += 1
         else : count+= 1
