e5951dd3e36e1bedb3165f0696499edd6d0d9c49
SungBeum <an4126@gmail.com>
2023-04-03 10:55:22 +0900

    04.03

--- 안성범/25344_행성 정렬_dont_solved.py
+++ 안성범/25344_행성 정렬_dont_solved.py
@@ -1,19 +1,16 @@
 import sys
+from math import gcd
 sys.stdin = open('input.txt','r')
 sys.stdout = open('output.txt', 'w')
 input = sys.stdin.readline
-def gcd(a, b):
-    if a < b:
-        a , b = b, a
-    while True:
-        r = a%b
-        if r == 0:
-            break
-        a, b = b, r
-    return b
 
 N = int(input().strip()) -2
 routine = list(map(int, input().split()))
+print(routine)
+_lcm = routine[0]
+for planet in routine[1:]:
+    _lcm = int(_lcm*planet / gcd(_lcm, planet))
+print(_lcm)
 # print(N)
 # for i in range(N-2):
 #     fir_planet = routine[i]
@@ -21,3 +18,13 @@ routine = list(map(int, input().split()))
 #     gcd_ = gcd(routine[i], routine[i+1])
 #     lcm = routine[i]/gcd_*routine[i+1]
 
+
+# def gcd(a, b):
+#     if a < b:
+#         a , b = b, a
+#     while True:
+#         r = a%b
+#         if r == 0:
+#             break
+#         a, b = b, r
+#     return b
