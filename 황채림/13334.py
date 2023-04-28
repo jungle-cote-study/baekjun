f008b8332486af52aab6f9a4e44b8b5bdc1088f4
황채림 <cofla159@naver.com>
2023-04-28 13:39:06 +0900

    채림 4/28 오답풀이

--- 황채림/13334.py
+++ 황채림/13334.py
@@ -6,7 +6,9 @@ temp = []
 stop = []
 for _ in range(n):
     t = list(map(int, stdin.readline().split()))
-    if t[0] > t[1]:
+    # 범위 안에 포함되기 시작하는 건 l_end >= 끝값일 때니까 끝값 기준으로 정렬
+    # 사실상 끝값과 시작값을 따로 보는 것
+    if t[0] < t[1]:
         t = [t[1], t[0]]
     temp.append(t)
     stop.append(t[0])
@@ -16,7 +18,7 @@ stop = sorted(list(set(stop)))
 
 commute = []
 for t in temp:
-    if t[1]-t[0] <= d:
+    if t[0]-t[1] <= d:
         heapq.heappush(commute,t)
 
 answer = 0
@@ -24,8 +26,8 @@ in_l = []
 for l_start in stop:
     l_end = l_start+d
 
-    while commute and commute[0][1] <= l_end:
-        heapq.heappush(in_l,heapq.heappop(commute)[0])
+    while commute and commute[0][0] <= l_end:
+        heapq.heappush(in_l,heapq.heappop(commute)[1])
     while in_l and in_l[0] < l_start:
         heapq.heappop(in_l)
     answer = max(answer, len(in_l))
