ed89f28936d96971f75b5ba56f8c844c8a426928
황채림 <cofla159@naver.com>
2023-05-12 11:51:04 +0900

    채림 5/12 1CA

---
+++ 황채림/2961.py
@@ -0,0 +1,22 @@
+from sys import stdin
+from itertools import combinations
+from functools import reduce
+
+n = int(stdin.readline())
+
+taste = []
+for _ in range(n):
+    taste.append(list(map(int,stdin.readline().split())))
+
+answer = 10**9
+for i in range(1, n+1):
+    combi = combinations(taste, i)
+    for c in combi:
+        s = [x[0] for x in c]
+        b = [x[1] for x in c]
+        s_total = reduce(lambda x,y: x*y, s, 1)
+        b_total = sum(b)
+        diff = abs(s_total-b_total)
+        answer = min(answer, abs(reduce(lambda x,y: x*y, s, 1)-sum(b)))
+        
+print(answer)
