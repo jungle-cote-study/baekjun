fb7a52a99ec7f248c5857fa362405c6e990af019
SungBeum <an4126@gmail.com>
2023-05-12 12:01:33 +0900

    05.12_don't solved

---
+++ 안성범/2961_도영이가_만든_맛있는_음식.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt','r')
+sys.stdout = open('output.txt','w')
+input = sys.stdin.readline
+# N : 재료의 개수
+N = int(input().strip())
+tastes = [list(map(int, input().split()))for _ in range(N)]
+print(tastes)
+ans = 1e9
+for taste in tastes:
+    if (taste[1]-taste[0] < ans):
+        ans = taste[1]-taste[0]
+print(ans) 
+# while True:
+#     food = []
+    
