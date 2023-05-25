e6c7e1959aafe8598185e5a3c92e77182954ef26
bimo <109029169+jihi32123@users.noreply.github.com>
2023-05-25 22:46:30 +0900

    Update 1753.py

--- 박지희/1753.py
+++ 박지희/1753.py
@@ -7,14 +7,14 @@ graph = [[] for _ in range(V + 1)]
 
 start_v = int(sys.stdin.readline())
 
-## 입력 받아서 방향 그래프 만들기
+## 입력 받아서 방향 그래프 만들기 O(E)
 for i in range(E):
     u, v, w = map(int, sys.stdin.readline().split())
     graph[u].append([v,w])
 dist_list = [sys.maxsize] * (V+1)  
 
 # 이제 시작지점을 큐에 넣고 반복문을 돈다.
-que = []
+que = [] 
 heapq.heappush(que, [0, start_v])
 dist_list[start_v] = 0 # 시작 지점의 비용은 0이다. 
 
@@ -23,9 +23,9 @@ while(que):
     min_dist, min_node = heapq.heappop(que)
     # 가장 비용이 작은 노드에서 갈 수 있는 방향 탐색하기
     for v, d in graph[min_node]:
-        dist_list[v] = min(dist_list[v], dist_list[min_node] + d)
-        
-        heapq.heappush(que, [d, v])
+        if dist_list[v] > dist_list[min_node] + d:
+            dist_list[v] = dist_list[min_node] + d
+            heapq.heappush(que, [d, v])
         
 for d in dist_list[1:]: 
     if d == sys.maxsize:
